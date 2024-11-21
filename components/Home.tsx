67a32f65c4b43cfd24b788a65987dbb3f0cd1513
Paradox <chuhan718@gmail.com>
2024-11-21 09:15:12 +0530

    upload data

--- components/Home.tsx
+++ components/Home.tsx
@@ -8,60 +8,60 @@ import { getStorage, deleteObject, ref, listAll } from "firebase/storage";
 import app from "@/app/libs/firebase";
 
 const Details= ({ langars }:any) => {
-  const [state, setState] = useState("");
-  const [district, setDistrict] = useState("");
-  const filtered = langars.filter((langar: any) => {
-    const matchesState = state ? langar.state === state : true;
-    const matchesDistrict = district ? langar.district === district : true;
-    return matchesState && matchesDistrict;
-  });
+  // const [state, setState] = useState("");
+  // const [district, setDistrict] = useState("");
+  // const filtered = langars.filter((langar: any) => {
+  //   const matchesState = state ? langar.state === state : true;
+  //   const matchesDistrict = district ? langar.district === district : true;
+  //   return matchesState && matchesDistrict;
+  // });
 
-  const handleState = (event: any) => {
-    setState(event.target.value);
-  };
+  // const handleState = (event: any) => {
+  //   setState(event.target.value);
+  // };
 
-  const handleDistrict = (event: any) => {
-    setDistrict(event.target.value);
-  };
+  // const handleDistrict = (event: any) => {
+  //   setDistrict(event.target.value);
+  // };
 
-  useEffect(() => {
-    const deleteData = async () => {
-      try {
-        const res = await fetch("/api/remove", { method: "DELETE" });
-        await res.json();
-      } catch (error) {
-        console.error("Failed to delete data:", error);
-      }
-    };
+  // useEffect(() => {
+  //   const deleteData = async () => {
+  //     try {
+  //       const res = await fetch("/api/remove", { method: "DELETE" });
+  //       await res.json();
+  //     } catch (error) {
+  //       console.error("Failed to delete data:", error);
+  //     }
+  //   };
 
-    const deleteImage = async () => {
-      try {
-        const existingImageUrls = langars.filter((item:any) => item.imgUrl).map((item:any )=> item.imgUrl);
-        const storage = getStorage(app);
-        const storageRef = ref(storage, '');
-        const allImages = await listAll(storageRef);
-        const deletePromises = allImages.items.map(async (imageRef) => {
-          if (!existingImageUrls.includes(imageRef.fullPath)) {
-            try {
-              await deleteObject(imageRef);
-              console.log("Image deleted from Firebase:", imageRef.fullPath);
-            } catch (error) {
-              console.error("Error deleting image from Firebase:", error);
-            }
-          }
-        });
-        await Promise.all(deletePromises)
-        console.log("All images not referenced in MongoDB have been deleted from Firebase.");
-      } catch (error) {
-        console.error("Error occurred during the deletion process:", error);
-      }
-    };
-    deleteData();
-    deleteImage();
-  }, []);
+  //   const deleteImage = async () => {
+  //     try {
+  //       const existingImageUrls = langars.filter((item:any) => item.imgUrl).map((item:any )=> item.imgUrl);
+  //       const storage = getStorage(app);
+  //       const storageRef = ref(storage, '');
+  //       const allImages = await listAll(storageRef);
+  //       const deletePromises = allImages.items.map(async (imageRef) => {
+  //         if (!existingImageUrls.includes(imageRef.fullPath)) {
+  //           try {
+  //             await deleteObject(imageRef);
+  //             console.log("Image deleted from Firebase:", imageRef.fullPath);
+  //           } catch (error) {
+  //             console.error("Error deleting image from Firebase:", error);
+  //           }
+  //         }
+  //       });
+  //       await Promise.all(deletePromises)
+  //       console.log("All images not referenced in MongoDB have been deleted from Firebase.");
+  //     } catch (error) {
+  //       console.error("Error occurred during the deletion process:", error);
+  //     }
+  //   };
+  //   deleteData();
+  //   deleteImage();
+  // }, []);
   return (
     <div>
-      <Container>
+      {/* <Container>
         <Row>
           <Col xs={6}>
             <FormControl
@@ -116,17 +116,17 @@ const Details= ({ langars }:any) => {
             </FormControl>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
       <Container>
         <Row>
-          {filtered.length === 0 ? (
+          {langars.length === 0 ? (
             <Col>
               <div className="text-center my-4">
                 <h4>No bhandara in your area</h4>
               </div>
             </Col>
           ) : (
-            filtered.map((item:any) => (
+            langars.map((item:any) => (
               <Col md={4} key={item.id}>
                 <div className="border shadow bg-blue-50 p-3 my-3 rounded">
                   <div className="flex justify-center items-center">
